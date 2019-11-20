af776a4fca4ec72524cac3f80682a0a5af667299
Matheus-tech <56124526+Matheus-tech@users.noreply.github.com>
2019-11-20 15:24:00 -0300

    Add files via upload

---
+++ exer1.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class exer1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int i,n;
+		
+		fato();
+		
+	}
+	
+	public static void fato() {
+		Scanner in = new Scanner(System.in);
+        var i, aux, aux1, fatorial;
+       
+        System.out.println("Insira o numero");
+        fatorial = in.nextInt();
+        
+
+        for (i = 1; i <= fatorial; i++) {
+            fatorial = fatorial * i;
+            aux = aux + fatorial;
+        }
+        System.out.println("O fatorial de: " + aux);	
+				
+			}
+			
+
+	}
+	
+	
+	
+	
+		public static void tabuada() {
+			Scanner in = new Scanner(System.in);
+			int i,r,n,v;
+			System.out.println("numero da tabuada");
+			n = in.nextInt();
+			System.out.println("Até onde vai");
+			v = in.nextInt();
+			for(i=1; i<=v;i++) {
+				r = n*i;
+				System.out.println(n+"x"+i+"="+r);
+				
+			}		
+	}
+		public static void sexoIdade() {
+			Scanner in = new Scanner(System.in);
+			int  sexo, idade, a;
+			
+			System.out.println("Insira seu sexo \n 1 = Masculino \n 2 = feminino");
+			sexo = in.nextInt();
+			System.out.println("Insira sua idade");
+			idade= in.nextInt();
+			
+			if(sexo == 1) {
+				if(idade >= 65) {
+					System.out.println("Você já pode se aposentar");
+				}else {
+					a = 65 - idade;
+					System.out.println("Falta "+a+" Anos para você se aposentar");
+				}
+			}else  {
+				if(idade >= 60) {
+					System.out.println("Você já pode se aposentar");
+				}else {
+					a = 60 - idade;
+					System.out.println("Falta "+a+" Anos para você se aposentar");
+				}
+			}
+		}
+
+			public static void potencia() {
+				Scanner in = new Scanner(System.in);
+				int n, r;
+					System.out.println("Digite um número");
+					n = in.nextInt();
+					
+					r = n*n;
+					System.out.println("A potência de "+n+" é: " +r);
+				
+			}
+			
+			
+	
+
+}
